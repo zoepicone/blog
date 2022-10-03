a15e3662fc97ffaba6fefe5d84d894c0dd982e18
Zoe Picone <zoe@zoepicone.dev>
2022-10-04 12:20:15 +1300

    Will automatically add back link, pandoc title block support

--- blog.rb
+++ blog.rb
@@ -7,7 +7,8 @@ def run
   Dir.glob("#{PATH}/*.md") do |entry|
     converted = File.basename(entry, ".md")
     File.open("#{PATH}/#{converted}.html", "w") do |f|
-      f.write PandocRuby.new(File.read(entry), :standalone, "--shift-heading-level-by=-1", "--css #{Dir.pwd}/#{PATH}/style.css", from: 'markdown').to_html
+      File.write(entry, "\n\n[Back](./)", File.size(entry), mode:'a') unless File.read(entry).include?("[Back](./)")
+      f.write PandocRuby.new(File.read(entry), :standalone, "--css #{Dir.pwd}/#{PATH}/style.css", from: 'markdown+pandoc_title_block').to_html
     end
   end
 
