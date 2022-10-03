ca788a700aa988ab0a95cfe217db2a86a634654f
Zoe Picone <zoe@zoepicone.dev>
2022-10-04 11:36:16 +1300

    Now gets stylesheet

--- blog.rb
+++ blog.rb
@@ -6,10 +6,12 @@ PATH = ARGV[0]
 def run
   Dir.glob("#{PATH}/*.md") do |entry|
     converted = File.basename(entry, ".md")
-    File.open("#{PATH}/#{converted}.html", "w") { |f|
-      f.write PandocRuby.new(File.read(entry), :standalone, from: 'markdown').to_html
-    }
+    File.open("#{PATH}/#{converted}.html", "w") do |f|
+      f.write PandocRuby.new(File.read(entry), :standalone, "--shift-heading-level-by=-1", "--css #{Dir.pwd}/#{PATH}/style.css", from: 'markdown').to_html
+    end
   end
+
+
 end
 
 run
