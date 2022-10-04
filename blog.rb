634106e30b6f17b1f481db240a86830350d9731a
Zoe Picone <zoe@zoepicone.dev>
2022-10-04 13:29:19 +1300

    Added table of contents

--- blog.rb
+++ blog.rb
@@ -1,5 +1,5 @@
 require 'pandoc-ruby'
-require 'nokogiri'
+require 'date'
 
 PATH = ARGV[0]
 
@@ -7,12 +7,32 @@ def run
   Dir.glob("#{PATH}/*.md") do |entry|
     converted = File.basename(entry, ".md")
     File.open("#{PATH}/#{converted}.html", "w") do |f|
-      File.write(entry, "\n\n[Back](./)", File.size(entry), mode:'a') unless File.read(entry).include?("[Back](./)")
+      File.write(entry, "\n\n[Back](./#{PATH}.html)", File.size(entry), mode:'a') unless File.read(entry).include?("[Back](./#{PATH}.html)")
       f.write PandocRuby.new(File.read(entry), :standalone, "--css #{Dir.pwd}/#{PATH}/style.css", from: 'markdown+pandoc_title_block').to_html
     end
   end
 
+  toc
+end
 
+def toc
+  files = []
+  Dir.glob("#{PATH}/*.md") do |entry|
+    date = Date.parse(IO.readlines(entry)[2][2..-1])
+    files << [entry, date]
+  end
+  files = files.sort_by(&:last).reverse()
+  tocfile = File.open("./#{PATH}.md", "w")
+  tocfile.puts("% Blogposts\n\n")
+  for file in files do
+    tocfile.puts("#{file[1]} - [#{IO.readlines(file[0])[0][2..-1].strip!}](#{PATH}/#{File.basename(file[0], ".md")}.html)\n\n")
+  end
+  tocfile.puts("[Back](./.)")
+  tocfile.close
+
+  File.open("./#{PATH}.html", "w") do |f|
+    f.write PandocRuby.new(File.read("./#{PATH}.md"), :standalone, "--css #{Dir.pwd}/#{PATH}/style.css", from: 'markdown+pandoc_title_block').to_html
+  end
 end
 
 run
