86ccea76ec4cd444043a189e5066e2924485e05b
Zoe Picone <zoe@zoepicone.dev>
2022-10-04 15:01:55 +1300

    Ran rubocop

--- blog.rb
+++ blog.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'pandoc-ruby'
 require 'date'
 
@@ -5,10 +7,14 @@ PATH = ARGV[0]
 
 def run
   Dir.glob("#{PATH}/*.md") do |entry|
-    converted = File.basename(entry, ".md")
-    File.open("#{PATH}/#{converted}.html", "w") do |f|
-      File.write(entry, "\n\n[Back](../#{PATH}.html)", File.size(entry), mode:'a') unless File.read(entry).include?("[Back](../#{PATH}.html)")
-      f.write PandocRuby.new(File.read(entry), :standalone, "--css style.css", from: 'markdown+pandoc_title_block').to_html
+    converted = File.basename(entry, '.md')
+    File.open("#{PATH}/#{converted}.html", 'w') do |f|
+      unless File.read(entry).include?("[Back](../#{PATH}.html)")
+        File.write(entry, "\n\n[Back](../#{PATH}.html)", File.size(entry),
+                   mode: 'a')
+      end
+      f.write PandocRuby.new(File.read(entry), :standalone, '--css style.css',
+                             from: 'markdown+pandoc_title_block').to_html
     end
   end
 
@@ -18,20 +24,22 @@ end
 def toc
   files = []
   Dir.glob("#{PATH}/*.md") do |entry|
-    date = Date.parse(IO.readlines(entry)[2][2..-1])
+    date = Date.parse(IO.readlines(entry)[2][2..])
     files << [entry, date]
   end
-  files = files.sort_by(&:last).reverse()
-  tocfile = File.open("./#{PATH}.md", "w")
+  files = files.sort_by(&:last).reverse
+  tocfile = File.open("./#{PATH}.md", 'w')
   tocfile.puts("% Blogposts\n\n")
-  for file in files do
-    tocfile.puts("#{file[1]} - [#{IO.readlines(file[0])[0][2..-1].strip!}](#{PATH}/#{File.basename(file[0], ".md")}.html)\n\n")
+  files.each do |file|
+    tocfile.puts("#{file[1]} - [#{IO.readlines(file[0])[0][2..]
+                 .strip!}](#{PATH}/#{File.basename(file[0], '.md')}.html)\n\n")
   end
-  tocfile.puts("[Back](./.)")
+  tocfile.puts('[Back](./.)')
   tocfile.close
 
-  File.open("./#{PATH}.html", "w") do |f|
-    f.write PandocRuby.new(File.read("./#{PATH}.md"), :standalone, "--css #{PATH}/style.css", from: 'markdown+pandoc_title_block').to_html
+  File.open("./#{PATH}.html", 'w') do |f|
+    f.write PandocRuby.new(File.read("./#{PATH}.md"), :standalone, "--css #{PATH}/style.css",
+                           from: 'markdown+pandoc_title_block').to_html
   end
 end
 
